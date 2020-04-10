e99f3e7dcdb8294c81232cb1a0382073aa17ac40
Katrina Hernandez <abqkatrina@gmail.com>
2020-04-10 10:05:02 -0600

    setups in progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    axios.get('localhost:5000/projects')
+    .then(res => {
+      console.log(res)
+      setData(res.data)})
+      .catch(err => console.log(err))
+  }, []);
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>API Sprint - express, api, react-app</p>
+      </header> 
+      <div className="container">
+        <ul>
+          <li>List of things</li>
+          {/* (data.map( (item) => {
+              <li>{item.title}</li>
+          })) */}
+        </ul>
+      </div>
     </div>
   );
 }
--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const helmet = require("helmet");
 const projectRouter = require("./projects/projectRouter.js");
 const actionRouter = require("./actions/actionRouter.js");
 const server = express();
+const cors = require('cors');
 
 server.use(express.json());
+server.use(cors());
+
 server.use( helmet(), logger);
 server.use("/api/actions", actionRouter);
 server.use("/api/projects", projectRouter);
